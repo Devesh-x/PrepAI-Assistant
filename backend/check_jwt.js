354ca25452f1b47dfda0f6116907356c15dfe6d5
Devesh-x <rajput.devesh12111@gmail.com>
2026-01-14 02:10:25 +0530

    Cleanup debug scripts

--- backend/check_jwt.js
+++
@@ -1,3 +0,0 @@
-require("dotenv").config({ override: true });
-console.log("JWT_SECRET present:", !!process.env.JWT_SECRET);
-if (process.env.JWT_SECRET) console.log("JWT_SECRET length:", process.env.JWT_SECRET.length);
